4327acc44ecfd4e72f4711365a2e23d76d8d4fb5
Leo Hetsch <leo.hetsch@gmail.com>
2013-09-17 00:52:40 +0200

    fix bug url

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var API_URL =  "https://api.instagram.com/v1/tags/crmaxv/media/recent?client_id=7359917ce0c44b26b15157dc9f64fbb5&count=8";
+var API_URL =  "https://api.instagram.com/v1/tags/crmaxv/media/recent?client_id=7359917ce0c44b26b15157dc9f64fbb5";
 
 $(document).ready(function(){
     gettingPics();
